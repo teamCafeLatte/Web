8a7a86cc9060f248fb816c1434e9fff7c576a66d
rlajiun <grace3207@naver.com>
2019-11-08 20:41:03 +0900

    친구js 추가

---
+++ web_project/routes/friend.js
@@ -0,0 +1,69 @@
+const   fs = require('fs');
+const   express = require('express');
+const   ejs = require('ejs');
+const   url = require('url');
+const   mysql = require('mysql');
+const   bodyParser = require('body-parser');
+const   multer = require('multer');
+const   upload = multer({dest: __dirname + '/../public/images/uploads/userprofile'});  // 업로드 디렉터리를 설정한다.
+const   router = express.Router();
+
+router.use(bodyParser.urlencoded({ extended: false }));
+
+const   db = mysql.createConnection({
+   host: 'localhost',        // DB서버 IP주소
+   port: 3306,               // DB서버 Port주소
+   user: 'root',            // DB접속 아이디
+   password: '1020',  // DB암호
+   database: 'ournote'         //사용할 DB명
+});
+
+// --------------- 유저 프로필 보기 기능 --------------------
+const PrintProfile = (req, res) => {
+  let htmlstream = '';
+  let sql_str;
+  const query = url.parse(req.url, true).query;
+
+  console.log(query.user);
+
+  if(req.session.auth){ // 로그인한 경우에만 처리한다
+    htmlstream = fs.readFileSync(__dirname + '/../views/header.ejs','utf8');  // 헤더부분
+    htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/navbar.ejs','utf8'); //메뉴
+    htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/userprofile.ejs','utf8'); // 프로필화면
+    htmlstream = htmlstream + fs.readFileSync(__dirname + '/../views/footer.ejs','utf8');  // Footer
+
+    sql_str = "SELECT userID, userPass, userName, userPhone, userPic, userInfo from user where userID = ?"; // 상품 검색 SQL
+
+    res.writeHead(200, {'Content-Type':'text/html; charset=utf8'});
+
+    db.query(sql_str, query.user, (error, results, fields) => {  // 사용자 검색 SQL실행
+      if(error) {res.status(562).end("PrintProfile: DB query is failed");}
+      else if (results.length <= 0) { // 조회된 정보가 없다면, 오류메시지 출력
+        htmlstream2 = fs.readFileSync(__dirname + '/../views/error.ejs','utf8');
+        res.status(562).end(ejs.render(htmlstream2, { 'title': 'Error',
+                          'warn_title':'조회 오류',
+                          'warn_message':'조회된 정보가 없습니다.',
+                          'return_url':'/' }));
+      }
+      else{
+        res.end(ejs.render(htmlstream,  { 'title' : 'Our Note',
+                                          'logurl': '/users/logout',
+                                          'loglabel': 'Logout',
+                                          'regurl': '/users/profile',
+                                          'reglabel': req.session.who,
+                                          userdata : results[0] }));  // 조회된 정보
+      }
+    });
+  }
+  else {
+    htmlstream = fs.readFileSync(__dirname + '/../views/error.ejs','utf8');
+        res.status(562).end(ejs.render(htmlstream, { 'title': 'Error',
+                          'warn_title':'접근 오류',
+                          'warn_message':'로그인이 필요합니다. 회원가입을 해주세요!',
+                          'return_url':'/' }));
+  }
+}
+
+router.get('/list', PrintProfile);     // 유저 프로필화면을 출력
+
+module.exports = router;
