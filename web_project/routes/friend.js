728e272e4261921e40fe772f5f545cfe5e5a1498
rlajiun <grace3207@naver.com>
2019-11-09 19:43:31 +0900

    수정

--- web_project/routes/friend.js
+++ web_project/routes/friend.js
@@ -37,13 +37,13 @@ const PrintFriendList = (req, res) => {
     res.writeHead(200, {'Content-Type':'text/html; charset=utf8'});
     db.query(sql_str, [query.user, query.user], (error, results, fields) => {  // 사용자 검색 SQL실행
         if(error) {res.status(562).end("PrintFriendList: DB query is failed");}
-        else if (results.length <= 0) { // 조회된 정보가 없다면, 오류메시지 출력
-            htmlstream2 = fs.readFileSync(__dirname + '/../views/error.ejs','utf8');
-            res.status(562).end(ejs.render(htmlstream2, { 'title': 'Error',
-                            'warn_title':'조회 오류',
-                            'warn_message':'조회된 친구가 없습니다. 친구를 추가해주세요!',
-                            'return_url':'/friend/add' }));
-        }
+        // else if (results.length <= 0) { // 조회된 정보가 없다면, 오류메시지 출력
+        //     htmlstream2 = fs.readFileSync(__dirname + '/../views/error.ejs','utf8');
+        //     res.status(562).end(ejs.render(htmlstream2, { 'title': 'Error',
+        //                     'warn_title':'조회 오류',
+        //                     'warn_message':'조회된 친구가 없습니다. 친구를 추가해주세요!',
+        //                     'return_url':'/friend/add' }));
+        // }
         else{
             res.end(ejs.render(htmlstream,  { 'title' : 'Our Note',
                                             'logurl': '/users/logout',
